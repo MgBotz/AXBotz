faaa85876335cca371701b753a6f5bae8b216cdb
MG-Botz <113174711+MgBotz@users.noreply.github.com>
2022-09-13 13:41:03 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895604670507]
-│ • Gopay  [0895604670507]
-│ • Dana  [0895604670507]
+│ • Pulsa [082171432358]
+│ • Gopay  [08xxxxxxxxxxxx]
+│ • Dana  [082171432358]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6282171432358
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
-`.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
+`.trim(), wm, 'OKEH', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['about']
 handler.command = /^dona(te|si)$/i
